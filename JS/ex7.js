e8181330d9140f4d9cd087d02d4a8e78475a5e6e
LaiaRodri <laia.rodriguez@enti.cat>
2023-10-04 20:44:02 +0200

    RGB done. Si posem 1 al prompt, ens afegeix el 0.

--- JS/ex7.js
+++ JS/ex7.js
@@ -5,6 +5,11 @@ function calculaRGB(){
     let userGreen = parseInt(prompt("Entra el valor green", "Exemple: 05"));
     let userBlue = parseInt(prompt("Entra el valor blue.", "Exemple: 35"));
 
+    let hexaRed = userRed.toString(16).padStart(2, "0");
+    let hexaGreen = userGreen.toString(16).padStart(2, "0");
+    let hexaBlue = userBlue.toString(16).padStart(2, "0");
 
+    let concatString = "#" + hexaRed + hexaGreen + hexaBlue;
 
+    console.log(concatString);
 }
