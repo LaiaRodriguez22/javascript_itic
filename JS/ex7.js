44a98645f2a05e1376c28a95b95bea5b58d2f4b2
LaiaRodri <laia.rodriguez@enti.cat>
2023-10-04 20:31:29 +0200

    Fent exercici set, rgb.

---
+++ JS/ex7.js
@@ -0,0 +1,10 @@
+//Demanar al usuari un color en format rgb -3 numeros, 3 prompts- i convertir-lo a hexadecimal. n.ToString(16)
+
+function calculaRGB(){
+    let userRed = parseInt(prompt("Entra entra el valor red", "Exemple: 15"));
+    let userGreen = parseInt(prompt("Entra el valor green", "Exemple: 05"));
+    let userBlue = parseInt(prompt("Entra el valor blue.", "Exemple: 35"));
+
+
+
+}
