247b21d9ce3673b660683ef98da1c09374865387
LaiaRodri <laia.rodriguez@enti.cat>
2023-10-09 17:21:26 +0200

    EX9 acabat

--- JS/ex9.js
+++ JS/ex9.js
@@ -8,6 +8,29 @@ document.body.appendChild(table)
 --*/
 
 function taulaDinamicaDOM(){
-    
+
+    let fil = prompt("Entra un numero.", "Exemple: 2");
+    let colum = prompt("Entra un numero.", "Exemple: 5");
+
+    const tabla = document.createElement("table");
+    const tablaBody = document.createElement("tbody");
+      
+    for (let i = 0; i < fil; i++) {
+        const fila = document.createElement("tr");
+      
+        for (let j = 0; j < colum; j++) {
+            const celda = document.createElement("td");
+            celda.style.border="1px solid red";
+            const cellText = document.createTextNode(`F: ${i}, COL: ${j}`);
+            celda.appendChild(cellText);
+            fila.appendChild(celda);
+        }
+
+        tablaBody.appendChild(fila);
+    }
+      
+    tabla.appendChild(tablaBody);
+    document.body.appendChild(tabla);
+    tabla.style.border="1px solid black";
 }
 
