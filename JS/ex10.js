7858d5c13be61d3434a4e9cbefed4cda2401fb92
LaiaRodri <laia.rodriguez@enti.cat>
2023-10-11 19:31:09 +0200

    Done ex10, now doing 11.

--- JS/ex10.js
+++ JS/ex10.js
@@ -7,5 +7,39 @@
 */
 
 function taulaMultiplicacio(){
-    
+    let fil = prompt("Entra un numero.", "Exemple: 2");
+    let colum = prompt("Entra un numero.", "Exemple: 5");
+
+    const tabla = document.createElement("table");
+    const tablaBody = document.createElement("tbody");
+      
+    for (let i = 1; i <= fil; i++) {
+        const fila = document.createElement("tr");
+      
+        for (let j = 1; j <= colum; j++) {
+            const celda = document.createElement("td");
+            celda.style.border="1px solid red";
+            const cellText = document.createTextNode(`${i * j}`);
+            celda.appendChild(cellText);
+            fila.appendChild(celda);
+        }
+
+        tablaBody.appendChild(fila);
+    }
+      
+    tabla.appendChild(tablaBody);
+    document.body.appendChild(tabla);
+    tabla.style.border="1px solid black";
+}
+
+/*
+    Fes una taula dinamica on es demana n de columnes de color darrera fila 
+    I printa una piramade tal que
+
+    ha de ser un numero imparell
+
+*/ 
+function taulaPiramide(){
+    let fil = prompt("Entra un numero.", "Exemple: 2");
+
 }
