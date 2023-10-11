ca6aeb7e285f2b03bd8494cd537cd9e8767f5ae9
LaiaRodri <laia.rodriguez@enti.cat>
2023-10-11 19:17:37 +0200

    New files ex10

---
+++ JS/ex10.js
@@ -0,0 +1,11 @@
+/*
+    Crear una taula de multiplicacions, demanar les files i columnes. 
+    1   2   3   4   5
+    2   4   6   8   10
+    3   6   9   12  15
+
+*/
+
+function taulaMultiplicacio(){
+    
+}
