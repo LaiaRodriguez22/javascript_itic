99699f0406806af273ae1f0a59a7f6ba4c2212fc
LaiaRodri <laia.rodriguez@enti.cat>
2023-10-15 12:46:12 +0200

    ex11 piramide feta

--- JS/ex10.js
+++ JS/ex10.js
@@ -10,8 +10,8 @@ function taulaMultiplicacio(){
     let fil = prompt("Entra un numero.", "Exemple: 2");
     let colum = prompt("Entra un numero.", "Exemple: 5");
 
-    const tabla = document.createElement("table");
-    const tablaBody = document.createElement("tbody");
+    const tble = document.createElement("table");
+    const tBody = document.createElement("tbody");
       
     for (let i = 1; i <= fil; i++) {
         const fila = document.createElement("tr");
@@ -24,12 +24,12 @@ function taulaMultiplicacio(){
             fila.appendChild(celda);
         }
 
-        tablaBody.appendChild(fila);
+        tBody.appendChild(fila);
     }
       
-    tabla.appendChild(tablaBody);
-    document.body.appendChild(tabla);
-    tabla.style.border="1px solid black";
+    tble.appendChild(tBody);
+    document.body.appendChild(tble);
+    tble.style.border="1px solid black";
 }
 
 /*
@@ -41,8 +41,45 @@ function taulaMultiplicacio(){
     3 FORS: Un for per alçada, i un per blancs, un per vermells i un altre més per blancs
 
 */ 
-function taulaPiramide(){
-    let numColumnes = prompt("Entra un numero impar per les columnes.", "Exemple: 3");
-    let numFiles = Math.floor((numColumnes-1)/2);
+function taulaPiramide() {
+    let x = parseInt(prompt("NUM COLS"));
+    //EM FALTA DIR QUE SI EL PROMT % 1, BREAK. QUE AIXO DEFET ES UN IF, ELSE
+    let y = Math.floor(x/2)+1 
+    const tble = document.createElement("table");
+    const tBody = document.createElement("tbody");
+    let blanc, vermell
+    blanc = Math.floor((x-1)/2)
+    vermell = 1
     
+    for (let a = 0; a < y; a++) {
+      const fila = document.createElement("tr");
+  
+      for (let b = 0; b < blanc; b++) {
+        createCella("white", fila)
+      }
+      for (let b = 0; b < vermell; b++) {
+        createCella("red", fila)
+      }
+      for (let b = 0; b < blanc; b++) {
+        createCella("white", fila)
+      }
+      vermell += 2
+      blanc--
+      tBody.appendChild(fila);
+    }
+  
+    tble.appendChild(tBody);
+    document.body.appendChild(tble);
+    tble.style.border="3px double purple";
+}
+
+function createCella(color, fila){
+    const cella = document.createElement("td");
+    cella.style.border="1px solid green"
+    cella.style.width="20px"
+    cella.style.height="20px"
+    cella.style.backgroundColor=color;
+    const cellTxt = document.createTextNode("");
+    cella.appendChild(cellTxt);
+    fila.appendChild(cella);
 }
