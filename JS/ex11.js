b85a57de148304ec1e1cf688fb86ed96882b4314
LaiaRodri <laia.rodriguez@enti.cat>
2023-10-16 17:39:32 +0200

    Taula Rombo conseguida. Reset variables abans del segon for.

--- JS/ex11.js
+++ JS/ex11.js
@@ -5,46 +5,67 @@
 */
 
 function crearTaulaRomb() {
-    let numCasellesVermelles = prompt("Entra un numero.", "Exemple: 2");
-    let x = numCasellesVermelles;
-    let y = Math.floor(x / 2) + 1;
+    let x = parseInt(prompt("NUM COLS"));
+    let y = Math.floor(x/2)+1;
 
-    const taula = document.createElement("table");
+    const tble = document.createElement("table");
     const tBody = document.createElement("tbody");
 
     let blanc, vermell;
-    blanc = Math.floor((x - 1) / 2);
+    blanc = Math.floor((x-1)/2)
     vermell = 1;
-
-    for (let a = 0; a < (y*2); a++) {
-        const fila = document.createElement("tr");
-
-        for (let b = 0; b < blanc; b++) {
-            createCella("white", fila);
-        }
-        for (let b = 0; b < vermell; b++) {
-            createCella("red", fila);
-        }
-        for (let b = 0; b < blanc; b++) {
-            createCella("white", fila);
-        }
-        vermell += 2;
-        blanc--;
-        tBody.appendChild(fila);
+    
+    for (let a = 0; a < y; a++) {
+      const fila = document.createElement("tr");
+  
+      for (let b = 0; b < blanc; b++) {
+        createCella("white", fila)
+      }
+      for (let b = 0; b < vermell; b++) {
+        createCella("red", fila)
+      }
+      for (let b = 0; b < blanc; b++) {
+        createCella("white", fila)
+      }
+      vermell += 2;
+      blanc--;
+      tBody.appendChild(fila);
     }
 
-    taula.appendChild(tBody);
-    document.body.appendChild(taula);
-    taula.style.margin = "auto";
-    taula.style.border = "3px double purple";
+    vermell = x-2;
+    blanc = 1;
+
+    for (let a = 0; a < (y-1); a++) {
+        
+        const fila = document.createElement("tr");
+    
+        for (let b = 0; b < blanc; b++) {
+          createCella("white", fila)
+        }
+        for (let b = 0; b < vermell; b++) {
+          createCella("red", fila)
+        }
+        for (let b = 0; b < blanc; b++) {
+          createCella("white", fila)
+        }
+        vermell-=2;
+        blanc++;
+        tBody.appendChild(fila);
+      }
+
+
+    tble.appendChild(tBody);
+    document.body.appendChild(tble);
+    tble.style.margin="auto";
+    tble.style.border="3px double purple";
 }
 
-function createCella(color, fila) {
+function createCella(color, fila){
     const cella = document.createElement("td");
-    cella.style.border = "1px solid green";
-    cella.style.width = "20px";
-    cella.style.height = "20px";
-    cella.style.backgroundColor = color;
+    cella.style.border="1px solid green"
+    cella.style.width="20px"
+    cella.style.height="20px"
+    cella.style.backgroundColor=color;
     const cellTxt = document.createTextNode("");
     cella.appendChild(cellTxt);
     fila.appendChild(cella);
