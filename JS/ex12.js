1fb386a98bfe6df130a2c954b0079b3a9d9df287
LaiaRodri <laia.rodriguez@enti.cat>
2023-10-18 20:09:32 +0200

    Bugged bcs of arxiu.exe.html

--- JS/ex12.js
+++ JS/ex12.js
@@ -1,7 +1,8 @@
 //Demanar un nom d'arxiu a l'usuari i tornar per consola l'extensió de l'arxiu.
 function arxiuExtensio(){
-    let x = prompt("Dona'm el nom d'arxiu i la extenció");
+    let x = prompt("Dona'm el nom d'arxiu i la extensió");
     let splitedUser = x.split(".");
     console.log("El nom de l'arxiu es: " + splitedUser[0]);
     console.log("L'extensio es: " + splitedUser[1]);
+                            //splitedUser.length-1
 }
